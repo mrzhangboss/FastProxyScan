33cf1ca76b73496bc6bef0f06f80e22881093a72
mrzhangboss <2529450174@qq.com>
2017-11-29 21:52:14 +0800

    Refactor scan
    1. rename start -> proxy (scan port), scan -> vps (scan ip)
    2. refactor
    
     - set is_deleted = True after scan vps port, and delete it after expired
     - only scan new ip for port
     - some functions renamed

--- pool/proxy_pool/scanner/management/commands/scan.py
+++ pool/proxy_pool/scanner/management/commands/scan.py
@@ -13,9 +13,10 @@ import requests
 import time
 from datetime import timedelta
 from os import path
-from django.utils.timezone import datetime
 from pprint import pprint
+from django.utils.timezone import datetime
 from django.core.management import BaseCommand
+from django.db.models import Q
 from database.models import HostInfo, IPInfo, Proxy, PROXY_STATE
 from scanner.scanner import PortScanner, BColors
 from scanner.apnic_parse import get_ip_dress
@@ -23,15 +24,12 @@ from scanner.apnic_parse import get_ip_dress
 DOMAIN_FMT = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\*$')
 
 
-def save_result(host, result, bigger):
-    defaults = {'mode': 2, 'is_deleted': True} if DOMAIN_FMT.search(host.strip()) else None
-    host_info, created = HostInfo.objects.get_or_create(host=host, defaults=defaults)
-    if created:  # set deleted is True, not scan next time
-        print(BColors.BOLD, 'add domain', host_info.host, BColors.END)
-    if not result['scan']:  # delete host when it no port open
-        print('no proxy scan', host)
-        host_info.is_deleted = True
-        host_info.save()
+def save_proxy(host, result):
+    # set deleted is True, not scan next time
+    host_info, created = HostInfo.objects.update_or_create(host=host, defaults={'is_deleted': True})
+    if created:
+        print(BColors.BOLD, 'add host', host_info.host, BColors.END)
+    if not result['scan']:
         return
     state = {v: k for k, v in PROXY_STATE}
     scan_stats = result['nmap']['scanstats']
@@ -48,12 +46,6 @@ def save_result(host, result, bigger):
         if created:
             print('insert one ip', ip)
         if ip_port_sum > 0:
-            if ip_port_sum >= bigger:
-                new_host, created = HostInfo.objects.update_or_create(host=ip_info.ip,
-                                                                      defaults={'port_sum': ip_port_sum})
-                if created:
-                    print('insert one host: %s port sum bigger than' % ip_info.ip, bigger)
-
             for port in result['scan'][ip]['tcp']:
                 p = result['scan'][ip]['tcp'][port]
                 proxy, created = Proxy.objects.update_or_create(
@@ -69,15 +61,41 @@ def save_result(host, result, bigger):
     host_info.save()
 
 
-async def host_scan(semaphore, host, port=None, bigger=15, exclude=None, timeout=360, sleep_time=0.5, origin=None):
+async def port_scan(semaphore, host, port=None, timeout=60, sleep_time=0.5):
     async with semaphore:
-        scanner = PortScanner(host, port=port, exclude=exclude, host_timeout=timeout)
+        scanner = PortScanner(host, port=port, host_timeout=timeout)
         scanner.scan()
         print(host, 'begin scan', datetime.now())
         while scanner.is_running:
             await asyncio.sleep(sleep_time)
         print(BColors.OK_GREEN, host, 'scan over', datetime.now(), BColors.END)
-        save_result(origin if origin else host, scanner.result, bigger)
+        save_proxy(host, scanner.result)
+
+
+def save_host(host, result):
+    defaults = {'mode': 2, 'is_deleted': True}
+    host_info, created = HostInfo.objects.get_or_create(host=host, defaults=defaults)
+    if created:  # set deleted is True, not scan next time
+        print(BColors.BOLD, 'add domain', host_info.host, BColors.END)
+    if not result['scan']:  # delete host when it no port open
+        print('no vps scan', host)
+        return
+    scan_stats = result['nmap']['scanstats']
+    host_speed = (int(scan_stats['end']) - int(scan_stats['start'])) * 100
+    ip_speed = host_speed // int(scan_stats['totalhosts'])
+    host_port_sum = 0
+    for ip in result['scan']:
+        ip_port_sum = len(result['scan'][ip]['tcp']) if result['scan'][ip].get('tcp') else 0
+        host_port_sum += ip_port_sum
+        host_info, created = HostInfo.objects.get_or_create(host=ip, defaults={
+            'speed': ip_speed,
+            'port_sum': ip_port_sum
+        })
+        if created:
+            print('insert one host', ip)
+    host_info.port_sum = host_port_sum
+    host_info.speed = host_speed
+    host_info.save()
 
 
 async def ip_scan(semaphore, host, timeout=1, sleep_time=0.5):
@@ -88,30 +106,7 @@ async def ip_scan(semaphore, host, timeout=1, sleep_time=0.5):
         while scanner.is_running:
             await asyncio.sleep(sleep_time)
         print(BColors.OK_GREEN, host, 'scan over', datetime.now(), BColors.END)
-        result = scanner.result
-        defaults = {'mode': 2, 'is_deleted': True}
-        host_info, created = HostInfo.objects.get_or_create(host=host, defaults=defaults)
-        if created:  # set deleted is True, not scan next time
-            print(BColors.BOLD, 'add domain', host_info.host, BColors.END)
-        if not result['scan']:  # delete host when it no port open
-            print('no vps scan', host)
-            return
-        scan_stats = result['nmap']['scanstats']
-        host_speed = (int(scan_stats['end']) - int(scan_stats['start'])) * 100
-        ip_speed = host_speed // int(scan_stats['totalhosts'])
-        host_port_sum = 0
-        for ip in result['scan']:
-            ip_port_sum = len(result['scan'][ip]['tcp']) if result['scan'][ip].get('tcp') else 0
-            host_port_sum += ip_port_sum
-            host_info, created = HostInfo.objects.get_or_create(host=ip, defaults={
-                'speed': ip_speed,
-                'port_sum': ip_port_sum
-            })
-            if created:
-                print('insert one host', ip)
-        host_info.port_sum = host_port_sum
-        host_info.speed = host_speed
-        host_info.save()
+        save_host(host, scanner.result)
 
 
 def get_host_domain(ip):
@@ -129,56 +124,38 @@ def get_domain_ips(ip):
 def pre_delete(day):
     now = datetime.now()
     expires_date = now - timedelta(days=day)
-    # delete host mode = 3 like 127.0.0.*
-    d_s = 0
-    for h in HostInfo.objects.filter(insert_at__lte=expires_date, mode=3):
-        d_s += 1
-        h.delete()
-    print('delete %d host' % d_s)
+    # delete host is_deleted = True
+    host_query = HostInfo.objects.filter(insert_at__lte=expires_date, is_deleted=True)
+    print('delete %d host' % host_query.count())
+    host_query.delete()
 
-    d_s = 0
     # delete proxy
-    for p in Proxy.objects.filter(is_checked=True, is_proxy=False, insert_at__lte=expires_date).all():
-        d_s += 1
-        p.delete()
-    print('delete %d proxy' % d_s)
+    proxy_query = Proxy.objects.filter(is_checked=True, is_proxy=False, insert_at__lte=expires_date)
+    print('delete %d proxy' % proxy_query.count())
+    proxy_query.delete()
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('--proxy', action='store_true')
-        parser.add_argument('--vps', action='store_true')
-        parser.add_argument('--force-run', action='store_true', dest='force', help="force run domain scan")
-        parser.add_argument('-m', '--max-size', action='store', dest='max_size', default=20, type=int)
-        parser.add_argument('-t', '--task-sum', action='store', dest='sum', default=1000, type=int)
-        parser.add_argument('-b', '--bigger', action='store', dest='bigger', default=15, type=int,
-                            help='if ip port sum bigger than it, will scan it domain')
+        parser.add_argument('--proxy', action='store_true', help='run nmap scan vps port scan')
+        parser.add_argument('--vps', action='store_true', help='run nmap scan vps ip')
+        parser.add_argument('-m', '--max-size', action='store', dest='max_size', default=50, type=int,
+                            help='the bigger number the highe speed')
+        parser.add_argument('-t', '--task-sum', action='store', dest='sum', default=200, type=int,
+                            help='vps scan task sun')
         parser.add_argument('--expires', '-e', action='store', dest='expires', default=2, type=int,
-                            help='day of the checked host and proxy')
+                            help='day of the expires of host and proxy')
 
     def handle(self, *args, **options):
         start = time.time()
         loop = asyncio.get_event_loop()
         semaphore = asyncio.Semaphore(options['max_size'])
-        bigger = options['bigger']
-        is_force_run = options['force']
         if options['proxy']:
             tasks = []
-            for host_info in HostInfo.objects.filter(is_deleted=False, mode=0).order_by(
-                    '-port_sum').all():  # for ip scan
-                tasks.append(asyncio.ensure_future(host_scan(semaphore, host_info.host, bigger=bigger)))
-                # continue
-                if host_info.port_sum > bigger:
-                    domain = get_host_domain(host_info.host)
-                    if not HostInfo.objects.filter(host=domain).exists() or is_force_run:
-                        ips = get_domain_ips(host_info.host)
-                        # if bigger than bigger // 2 , we add it to our host to scan
-                        for ip in ips:
-                            tasks.append(
-                                asyncio.ensure_future(host_scan(semaphore, ip, bigger=bigger // 2, origin=domain)))
+            for host_info in HostInfo.objects.exclude(
+                            Q(mode=2) | Q(is_deleted=True)).all():  # for not domain ip like 127.0.0.*
+                tasks.append(asyncio.ensure_future(port_scan(semaphore, host_info.host)))
 
-            for host_info in HostInfo.objects.filter(is_deleted=False, mode=1).all():  # for domain scan
-                tasks.append(asyncio.ensure_future(host_scan(semaphore, host_info.host, bigger=bigger)))
             loop.run_until_complete(asyncio.wait(tasks))
             print(BColors.OK_GREEN, len(tasks), 'tasks over', datetime.now(), 'cost ', time.time() - start, 's')
 
@@ -194,8 +171,8 @@ class Command(BaseCommand):
             ip_group = get_ip_dress(filename=fn)
             random.shuffle(ip_group)
             for domain in ip_group:
-                if not HostInfo.objects.filter(host=domain).exists() or is_force_run:
-                    tasks.append(asyncio.ensure_future(ip_scan(semaphore, domain, sleep_time=5)))
+                if not HostInfo.objects.filter(host=domain).exists():
+                    tasks.append(asyncio.ensure_future(ip_scan(semaphore, domain, sleep_time=1)))
                     if len(tasks) > tasks_sum:
                         break
             if not tasks:
